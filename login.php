ae7b126cff005c14770144fc947e543acf0b8296
MarlenaFlueh <mf@esyoil.com>
2018-07-05 09:36:09 +0200

    add login page

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./style.css">
+  <title>diaflix</title>
+</head>
+
+<body class="LandingPage">
+  <header>
+    <nav>
+      <a href="/">diaflix-logo</a>
+    </nav>
+  </header>
+  <main class="LandingPage">
+    <div class="FullDiv">
+      <div class="BorderDiv">
+        <form>
+          <div>
+            <p class="LoginHeading">Einloggen</p>
+          </div>
+          <div class="Input">
+            <input type="text" name="your username" value="username" class="InputElement">
+          </div>
+          <div class="Input">
+            <input type="password" name="your password" value="password" class="InputElement">
+          </div>
+          <div class="FinalLogin">
+            <a href="/ ">
+              Einloggen
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
