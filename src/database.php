f9e42427829c7888866d61c7014a0151a282da56
MarlenaFlueh <mf@esyoil.com>
2018-07-09 07:57:30 +0200

    add database password

--- src/database.php
+++ src/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=diary;charset=utf8', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=diary;charset=utf8', 'diary', 'PzjZJ4g5dfV623R1');
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 function fetchEntries()
