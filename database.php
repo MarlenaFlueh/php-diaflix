bc567e2ad2321423f9628ef69623cd33d648d615
MarlenaFlueh <mf@esyoil.com>
2018-07-10 09:52:27 +0200

    reduce database.php

--- database.php
+++ database.php
@@ -3,10 +3,4 @@
 $pdo = new PDO('mysql:host=localhost;dbname=diary;charset=utf8', 'diary', 'PzjZJ4g5dfV623R1');
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-function fetchEntries()
-{
-    global $pdo;
-    return $pdo->query("SELECT * FROM `entries`");
-}
-
 ?>
