90b8a3d1cff45d846421f512a74dae1ad0e738b0
MarlenaFlueh <mf@esyoil.com>
2018-07-11 10:08:46 +0200

    add render functions

--- src/Entries/EntryController.php
+++ src/Entries/EntryController.php
@@ -4,9 +4,29 @@ namespace App\Entries;
 
 class EntryController
 {
-    function fetchEntries($pdo)
+    protected function render($view, $parameter)
+    {
+        foreach ($parameter as $key => $value) {
+            ${$key} = $value;
+        }
+
+        include __DIR__ . "/../../views/entries/{$view}.php";
+    }
+
+    protected function renderSite($view)
+    {
+        include __DIR__ . "/../../views/entries/{$view}.php";
+    }
+
+    public function fetchEntries($pdo)
     {
         $allEntries = new EntryModel($pdo);
-        return $allEntries->getAllEntries();
+        $entries = $allEntries->getAllEntries();
+        $this->render("entries", ['entries' => $entries]);
+    }
+
+    public function index()
+    {
+        $this->renderSite("index");
     }
 }
