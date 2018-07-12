4e5875ea5039bb4859d4221e894746751ff4eb16
MarlenaFlueh <mf@esyoil.com>
2018-07-12 10:35:14 +0200

    create method for setting new entries

--- src/Entries/EntryModel.php
+++ src/Entries/EntryModel.php
@@ -12,6 +12,18 @@ class EntryModel
         $this->pdo = $pdo;
     }
 
+    public function setNewEntry($title, $entry)
+    {
+        $stmt = $this->pdo->prepare(
+            "INSERT INTO `entries` (`user_id`, `title`, `entry`) VALUES (:user_id, :title, :entry)"
+        );
+        $stmt->execute([
+            'user_id' => 1,
+            'title' => $title,
+            'entry' => $entry
+        ]);
+    }
+
     public function getAllEntries()
     {
         return $this->pdo->query("SELECT * FROM `entries`");
