2b6c96b802095e08d44f5dc01034859bce9f5cb4
MarlenaFlueh <mf@esyoil.com>
2018-07-13 12:22:18 +0200

    add session logik

--- src/Users/LoginController.php
+++ src/Users/LoginController.php
@@ -12,9 +12,26 @@ class LoginController
         include __DIR__ . "/../../views/users/{$view}.php";
     }
 
+    public function checkSessionStatus($pdo)
+    {
+        if (isset($_SESSION['login'])) {
+            $entryModel = new EntryController();
+            $entryModel->fetchEntries($pdo);
+        } else {
+            echo "Just available for memebers.";
+        }
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['login']);
+        session_regenerate_id(true);
+        header("Location: index");
+    }
+
     public function login($pdo)
     {
-       /* var_dump(password_hash("test", PASSWORD_DEFAULT)); */
+       /* var_dump(password_hash("test", PASSWORD_DEFAULT)); test-username: Merle */
         $error = null;
         $model = new UserModel($pdo);
         if (!empty($_POST['username']) AND !empty($_POST['password'])) {
@@ -23,8 +40,9 @@ class LoginController
             $user = $model->getSingleUser($username);
             if (!empty($user)) {
                 if (password_verify($password, $user->password)) {
-                   echo "Login successful!";
-                   die();
+                    $_SESSION['login'] = $user->username;
+                    session_regenerate_id(true);
+                   header("Location: entries");
                 } else {
                     $error = "Incorrect password.";
                 }
