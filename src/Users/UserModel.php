ab931dd9367e2121f2605fb65b136847dec971e0
MarlenaFlueh <mf@esyoil.com>
2018-07-23 10:26:56 +0200

    create newUser method

--- src/Users/UserModel.php
+++ src/Users/UserModel.php
@@ -14,10 +14,19 @@ class UserModel
 
     public function getSingleUser($username)
     {
-       $stmt = $this->pdo->prepare("SELECT `username`, `password` FROM `users` WHERE username = :username");
-       $stmt->execute(['username' => $username]);
-       $user = $stmt->fetch(PDO::FETCH_OBJ);
-       return $user;
+        $stmt = $this->pdo->prepare("SELECT `username`, `password` FROM `users` WHERE username = :username");
+        $stmt->execute(['username' => $username]);
+        $user = $stmt->fetch(PDO::FETCH_OBJ);
+        return $user;
+    }
+
+    public function newUser($username, $password)
+    {
+        $stmt = $this->pdo->prepare("INSERT INTO `users` (`username`, `password`) VALUES (:username, :password)");
+        $stmt->execute([
+            'username' => $username,
+            'password' => $password
+        ]);
     }
 }
 
